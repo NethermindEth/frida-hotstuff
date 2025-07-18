e9844de42e36fc14861c7644803339c633d045df
Chee Chyuan Ang <cheechyuanang@Chees-MacBook-Pro-2.local>
2025-07-18 16:05:04 +0800

    remove prints

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -3,7 +3,7 @@ use frida_app::{create_app as create_frida_app, network::mock_network as mock_ne
 use frida_poc::winterfell::FriOptions;
 
 fn main() {
-    let frida_benchmark_file_path = "output/frida-benchmark.txt";
+    let frida_benchmark_file_path = "frida-benchmark.txt";
     let num_of_validators = vec![3, 5, 10, 20, 50, 100];
     let data_sizes = vec![(100, 100), (1000, 1000), (10_000, 10_000)];
     let fri_options = vec![FriOptions::new(2, 2, 1)];
