fb5a2d807553fa2fa7d636cea1f70cac654719c0
Matteo Lisotto <matteo.lisotto@gmail.com>
2025-07-29 15:52:40 +0200

    Merge `benchmark` with `benchmark-framework`
    Next commit will rename `benchmark-framework` to `benchmark`

--- benchmark-framework/src/main.rs
+++ benchmark-framework/src/main.rs
@@ -3,7 +3,7 @@ use frida_app::{create_app as create_frida_app, network::mock_network as mock_ne
 use frida_poc::winterfell::FriOptions;
 
 fn main() {
-    let _guard = logging::init_logging();
+    let guard = logging::init_logging();
     let num_of_validators = vec![3, 5, 10, 20, 50, 100];
     let data_sizes = vec![(100, 100), (1000, 1000), (10_000, 10_000)];
     let fri_options = vec![FriOptions::new(2, 2, 1)];
@@ -16,12 +16,12 @@ fn main() {
         frida_benchmark_file_path,
     );
 
-    let defrida_benchmark_file_path = "defrida-benchmark.txt";
+    let _defrida_benchmark_file_path = "defrida-benchmark.txt";
     // benchmark.start(
     //     |peers| mock_network_defrida_app(peers.cloned()),
     //     create_defrida_app,
     //     defrida_benchmark_file_path,
     // );
 
-    drop(_guard);
+    drop(guard);
 }
